d288deb243922815034ceb8b9d686b52d41b8551
jojohnson-jsj <jojohnson.jsj@gmail.com>
2024-11-17 16:15:02 -0800

    Adding startup.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,72 +1,11 @@
-using Azure.Identity;
-using LuxuryPaintJohnsonAPI.Data;
-using LuxuryPaintJohnsonAPI.Repositories;
-using LuxuryPaintJohnsonAPI.Services;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
-var azureCredential = new DefaultAzureCredential();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-	var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-	options.UseSqlServer(connectionString, sqlOptions =>
-	{
-		sqlOptions.EnableRetryOnFailure();
-	});
-});
-
-// Dependency Injection
-builder.Services.AddScoped<IPhotoService, PhotoService>();
-builder.Services.AddScoped<IPhotoRepository, PhotoRepository>();
-builder.Services.AddScoped<IProjectService, ProjectService>();
-builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
-
-// Register AutoMapper
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-// Add services to the container
-builder.Services.AddControllers();
-
-// Add CORS policy
-builder.Services.AddCors(options =>
-{
-	options.AddPolicy("AllowSpecificOrigins", policy =>
-	{
-		policy.WithOrigins("http://localhost:5173")
-			  .AllowAnyHeader()
-			  .AllowAnyMethod();
-	});
-});
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-	c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-	{
-		Title = "LuxuryPaintJohnsonAPI",
-		Version = "v1"
-	});
-});
+// Add Startup configuration
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI(c =>
-	{
-		c.SwaggerEndpoint("/swagger/v1/swagger.json", "LuxuryPaintJohnsonAPI V1");
-		c.RoutePrefix = string.Empty;
-	});
-}
-
-app.UseStaticFiles();
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
+startup.Configure(app, app.Environment);
 
 app.Run();
---
+++ src/Startup.cs
@@ -0,0 +1,79 @@
+﻿using Azure.Identity;
+using LuxuryPaintJohnsonAPI.Data;
+using LuxuryPaintJohnsonAPI.Repositories;
+using LuxuryPaintJohnsonAPI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+public class Startup
+{
+	public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+
+	public IConfiguration Configuration { get; }
+
+	public void ConfigureServices(IServiceCollection services)
+	{
+		var azureCredential = new DefaultAzureCredential();
+
+		services.AddDbContext<AppDbContext>(options =>
+		{
+			var connectionString = Configuration.GetConnectionString("DefaultConnection");
+			options.UseSqlServer(connectionString, sqlOptions =>
+			{
+				sqlOptions.EnableRetryOnFailure();
+			});
+		});
+
+		services.AddScoped<IPhotoService, PhotoService>();
+		services.AddScoped<IPhotoRepository, PhotoRepository>();
+		services.AddScoped<IProjectService, ProjectService>();
+		services.AddScoped<IProjectRepository, ProjectRepository>();
+
+		services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+		services.AddControllers();
+		services.AddCors(options =>
+		{
+			options.AddPolicy("AllowSpecificOrigins", policy =>
+			{
+				policy.WithOrigins("http://localhost:5173")
+					  .AllowAnyHeader()
+					  .AllowAnyMethod();
+			});
+		});
+		services.AddEndpointsApiExplorer();
+		services.AddSwaggerGen(c =>
+		{
+			c.SwaggerDoc("v1", new OpenApiInfo
+			{
+				Title = "LuxuryPaintJohnsonAPI",
+				Version = "v1"
+			});
+		});
+	}
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		if (env.IsDevelopment() || env.IsProduction())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "LuxuryPaintJohnsonAPI V1");
+				c.RoutePrefix = string.Empty;
+			});
+		}
+
+		app.UseStaticFiles();
+		app.UseHttpsRedirection();
+		app.UseRouting();
+		app.UseAuthorization();
+
+		app.UseEndpoints(endpoints =>
+		{
+			endpoints.MapControllers(); // Correctly maps controller routes
+		});
+	}
+}
